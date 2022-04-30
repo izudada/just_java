d8a36fe26b4a98b46635382f269e7e9b48c28868
Anthony <tonyudeagbala@gmail.com>
2022-04-30 17:46:43 +0100

    first commit

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.justjava;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+    int quantity = 5;
+        int price = 10;
+        display(quantity);
+        displayPrice(quantity * price);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        Locale nigeria = new Locale("en", "NG");
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance(nigeria).format(number));
+    }
+}
