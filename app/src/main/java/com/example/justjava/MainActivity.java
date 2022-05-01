685ea93dd46e4fe229f2f119f92212db07847f81
Anthony <tonyudeagbala@gmail.com>
2022-05-01 18:40:58 +0100

    Edit quantityTextView in display

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(number);
     }
 
     /**
