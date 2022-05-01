501b594c8147019fab5e91696d83ea9f4925f65f
Anthony <tonyudeagbala@gmail.com>
2022-05-01 17:53:35 +0100

    Edit button margin

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -46,4 +46,12 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance(nigeria).format(number));
     }
+
+    /**
+        This is a method to increase the quantity by 1
+     */
+    public void increment() {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("");
+    }
 }
